ffabbf564e61c43c21254484d68cd99e3e6b4ced
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2023-10-12 00:56:53 +0900

    Impl Streaming logic

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
-from typing import Union
 from fastapi import FastAPI, WebSocket
-from fastapi.responses import HTMLResponse
-from fastapi_socketio import SocketManager
+import asyncio
+import pandas as pd
+import librosa
+import scipy
+from scipy import signal
 
 app = FastAPI()
+
+FS = 44100
+## Music data
+AudioFile, FSAudio = librosa.load('soundhelix_mono.mp3')
+SamplingNum = int(AudioFile.size * FS / FSAudio)
+AudioFile = scipy.signal.resample(AudioFile, SamplingNum)
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
-    while True:
-        data = await websocket.receive_text()
-        await websocket.send_text(f"Message text was: {data}")
+        
+    chunk_size = 1024
+    
+    for i in range(0, len(AudioFile), chunk_size):
+        chunk1 = AudioFile[i:i+chunk_size].tobytes()
+        chunk2 = AudioFile[i:i+chunk_size].tobytes()
+        await websocket.send_bytes(chunk1 + chunk2)
+    
+    await websocket.close()
