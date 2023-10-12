3393c3f5d7b9713d154da7e936bc162e6590478b
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2023-10-12 15:38:11 +0900

    Impl async await json

--- main.py
+++ main.py
@@ -9,19 +9,32 @@ app = FastAPI()
 
 FS = 44100
 ## Music data
-AudioFile, FSAudio = librosa.load('soundhelix_mono.mp3')
+AudioFile, FSAudio = librosa.load('Source/mp3/soundhelix_mono.mp3')
 SamplingNum = int(AudioFile.size * FS / FSAudio)
 AudioFile = scipy.signal.resample(AudioFile, SamplingNum)
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
-        
+
     chunk_size = 1024
     
     for i in range(0, len(AudioFile), chunk_size):
-        chunk1 = AudioFile[i:i+chunk_size].tobytes()
-        chunk2 = AudioFile[i:i+chunk_size].tobytes()
-        await websocket.send_bytes(chunk1 + chunk2)
-    
+        
+        # 여기서 HRTFEffect 함수
+        left = AudioFile[i:i+chunk_size].tobytes()
+        right = AudioFile[i:i+chunk_size].tobytes()
+        
+        await websocket.send_bytes(left + right)
+        
+        try:
+            new_data = await asyncio.wait_for(websocket.receive_json(), timeout=0.02)
+
+            if new_data:
+                print(new_data)
+                # 후처리
+            
+        except asyncio.TimeoutError:
+            pass
+        
     await websocket.close()
