727f1bb8b36977f7cd45298855dbd0c2cbddb7dd
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2023-10-17 04:31:48 +0900

    optimize logic

--- main.py
+++ main.py
@@ -23,9 +23,19 @@ AudioFile, FSAudio = librosa.load('Source/mp3/soundhelix_mono.mp3')
 SamplingNum = int(AudioFile.size * FS / FSAudio)
 AudioFile = scipy.signal.resample(AudioFile, SamplingNum)
 
-# if AudioFile.size % HopSize != 0:
-#     remain = HopSize - AudioFile.size % HopSize
-#     AudioFile = np.append(AudioFile, np.zeros(remain), axis = 0)
+remainder = len(AudioFile) % HopSize
+if remainder != 0:
+    pad_length = HopSize - remainder
+    AudioFile = np.pad(AudioFile, (0, pad_length), 'constant', constant_values = 0)
+
+## Buffer setup
+outBufferL = np.zeros((FS + FrameSize, 1))
+outBufferR = np.zeros((FS + FrameSize, 1))
+
+inBuffer = np.zeros(FrameSize)
+    
+soundOutL = np.zeros((HopSize, 1))
+soundOutR = np.zeros((HopSize, 1))
 
 ## Source Position
 SourcePosition = pd.read_csv('Source/csv/SourcePosition.csv')
@@ -33,25 +43,18 @@ NumElePosition = HRTF_Effect.divideSourcePostion(SourcePosition)
 
 headRot = [0, 0, 0]
 soundSource = [112, 40]
+# INTERVAL = HopSize / FS 
+INTERVAL = 0.011
+
+HRIR_L_INT, HRIR_R_INT = HRTF_Effect.InterpolationHRIR(HRIR_L, HRIR_R, headRot, soundSource, NumElePosition)
+
+current_task = None
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
+    global HRIR_L_INT, HRIR_R_INT, outBufferL, outBufferR, inBuffer, soundOutL, soundOutR, headRot
+    
     await websocket.accept()
-    
-    ## Buffer setup
-    outBufferNum = FS + FrameSize
-    outBufferL = np.zeros((outBufferNum, 1))
-    outBufferR = np.zeros((outBufferNum, 1))
-
-    inBufferNum = FrameSize
-    inBuffer = np.zeros(inBufferNum)
-    
-    soundOutL = np.zeros((HopSize, 1))
-    soundOutR = np.zeros((HopSize, 1))
-    
-    first_data = await websocket.receive_json()
-    headRot = [first_data['roll'], first_data['pitch'], first_data['yaw']]
-    HRIR_L_INT, HRIR_R_INT = HRTF_Effect.InterpolationHRIR(HRIR_L, HRIR_R, headRot, soundSource, NumElePosition)
     
     for i in range(0, len(AudioFile), HopSize):        
         try:
@@ -59,33 +62,42 @@ async def websocket_endpoint(websocket: WebSocket):
             
             new_data = await asyncio.wait_for(websocket.receive_json(), timeout=0.004)
             
-            new_headRot = [new_data['roll'], new_data['pitch'], new_data['yaw']]
-            
-            if new_data and (
-                abs(headRot[0] - new_headRot[0]) >= 1 
-                or abs(headRot[1] - new_headRot[1]) >= 1 
-                or abs(headRot[2] - new_headRot[2]) >= 1
-            ):
-                headRot = new_headRot
-            
-                HRIR_L_INT, HRIR_R_INT = HRTF_Effect.InterpolationHRIR(HRIR_L, HRIR_R, headRot, soundSource, NumElePosition)
-            
-            soundOutL, soundOutR, inBuffer, outBufferL, outBufferR = HRTF_Effect.HRTFEffect(HRIR_L_INT, HRIR_R_INT, AudioFile[i:i+HopSize], inBuffer, outBufferL, outBufferR)
+            if new_data and any(abs(headRot[j] - new_data[axis]) >= 5 for j,axis in enumerate(['roll', 'pitch', 'yaw'])):
+                headRot = [new_data['roll'], new_data['pitch'], new_data['yaw']]
+                schedule_update_HRIR()
 
         except asyncio.TimeoutError:
-            soundOutL, soundOutR, inBuffer, outBufferL, outBufferR = HRTF_Effect.HRTFEffect(HRIR_L_INT, HRIR_R_INT, AudioFile[i:i+HopSize], inBuffer, outBufferL, outBufferR)
             pass
         
-        left = b''.join(struct.pack('f', value) for value in [coord[0] for coord in soundOutL])
-        right = b''.join(struct.pack('f', value) for value in [coord[0] for coord in soundOutR])
+        soundOutL, soundOutR, inBuffer, outBufferL, outBufferR = HRTF_Effect.HRTFEffect(HRIR_L_INT, HRIR_R_INT, AudioFile[i:i+HopSize], inBuffer, outBufferL, outBufferR)
+        
+        left_bytes = struct.pack(f'{len(soundOutL)}f', *soundOutL.flatten())
+        right_bytes = struct.pack(f'{len(soundOutR)}f', *soundOutR.flatten())
 
-        await websocket.send_bytes(left + right)
+        await websocket.send_bytes(left_bytes + right_bytes)
 
         end = time.time()
-        elapsed_time = end - start
-    
-        if elapsed_time < 0.01160997732:
-            await asyncio.sleep(0.01160997732 - elapsed_time)
-        print("time:", start - end)
+        await waitInterval(start, end)
         
     await websocket.close()
+    
+async def waitInterval(start, end):
+    elapsed_time = end - start
+        
+    if elapsed_time < INTERVAL:
+        await asyncio.sleep(INTERVAL - elapsed_time)
+    else:
+        print("time over", INTERVAL, elapsed_time)
+        
+async def update_HRIR():
+    global HRIR_L_INT, HRIR_R_INT
+    
+    HRIR_L_INT, HRIR_R_INT = HRTF_Effect.InterpolationHRIR(HRIR_L, HRIR_R, headRot, soundSource, NumElePosition)
+    
+def schedule_update_HRIR():
+    global current_task
+    
+    if current_task:
+        current_task.cancel()
+    
+    current_task = asyncio.create_task(update_HRIR())
